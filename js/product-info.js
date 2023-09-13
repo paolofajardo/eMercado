f092f5ecaceceb0422452d519d6cc66ba21f9c1d
nicocamelia <nicolas_camelia@hotmail.com>
2023-09-13 15:58:56 -0300

    arreglo en funcionalidad comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -142,17 +142,8 @@ let htmlContentToAppend = ''
 //    let checkedStars = Math.floor(newStarContenedor);
 //    let starContenedor = '<div class="stars-container">';
 //    let emptyStars = 5 - checkedStars;
-function getUser() {
-    let user = '';
-    let usuario = localStorage.getItem('usuario');
-    if (usuario !== null && usuario !== "") {
-      user = JSON.parse(usuario);
-    } else {
-      user = JSON.parse(sessionStorage.getItem('usuario'));
-    }
-    return user;
-  }
-let newUser = getUser()
+
+let newUser = JSON.parse(localStorage.getItem('usuario') || sessionStorage.getItem('usuario'))
 const newDateTime = new Date().toISOString()
 const newDescription = document.getElementById('comment-product').value;
 let starRate = document.getElementById('rate-star').value;
