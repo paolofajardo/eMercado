2c235771291b3c6f7378340b6b520662a70c914f
Paolo Fajardo <paolofajardo@outlook.com>
2023-11-26 01:32:34 -0300

    reestructuracion de carpetas

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const fs = require('fs');
 const path = require('path');
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 
 
 const app = express();
 const port = 3000;
+
+const secretKey = 'chicheKey';
 
 app.use(express.json())
 app.use(cors())
@@ -18,6 +21,18 @@ app.use(cors())
 
 app.use(express.static('emercado-api-main'));
 app.use(express.static('frontend'));
+
+// Endpoint POST /login
+app.post('/login', (req, res) => {
+  const { usuario } = req.body;
+
+  if (usuario.email && usuario.pass) {
+      const token = jwt.sign({ usuario }, secretKey, { expiresIn: '1h' });
+      res.status(200).json({ mensaje: 'Autenticación exitosa', token });
+  } else {
+      res.status(401).json({ mensaje: 'Autenticación fallida' });
+  }
+});
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
