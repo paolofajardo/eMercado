239c47e41551ca89b4e6034a533d23e3e2a3a378
Paolo Fajardo <paolofajardo@outlook.com>
2023-11-26 04:04:06 -0300

    intentando implementar jwt

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,13 @@ const jwt = require('jsonwebtoken');
 
 
 const app = express();
-const port = 3000;
+const port = 5500;
 
 const secretKey = 'chicheKey';
 
 app.use(express.json())
-app.use(cors())
+
+app.use(cors());
 
 /* app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -26,12 +27,16 @@ app.use(express.static('frontend'));
 app.post('/login', (req, res) => {
   const { usuario } = req.body;
 
-  if (usuario.email && usuario.pass) {
-      const token = jwt.sign({ usuario }, secretKey, { expiresIn: '1h' });
+  if (usuario) {
+      const token = jwt.sign({usuario}, secretKey, { expiresIn: '1h' });
       res.status(200).json({ mensaje: 'Autenticación exitosa', token });
   } else {
       res.status(401).json({ mensaje: 'Autenticación fallida' });
   }
+});
+
+app.get('/prueba', (req,res) => {
+  res.send('esto es una mierda')
 });
 
 app.listen(port, () => {
